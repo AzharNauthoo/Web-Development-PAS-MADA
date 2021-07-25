0a6347a58a1cc6a5597ad8be9be8503c739226d5
AzharNauthoo <61836941+AzharNauthoo@users.noreply.github.com>
2021-07-25 13:44:10 +0400

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp1.Models;
+using RegisterAndLoginApp1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginApp1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(Usermodel usermodel)
+        {
+            SecurityService securityService = new SecurityService();
+
+            if (securityService.IsValid(usermodel))
+            {
+                return View("LoginSuccess", usermodel);
+            }else
+            {
+                return View("LoginFailure", usermodel);
+            }
+            
+        }
+    }
+}
---
+++ SecurityService.cs
@@ -0,0 +1,27 @@
+﻿using RegisterAndLoginApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginApp1.Services
+{
+    public class SecurityService
+    {
+       
+        UsersDAO usersDAO = new UsersDAO();
+
+        public SecurityService()
+        {
+            
+
+        }
+
+        public bool IsValid(Usermodel user)
+        {
+            return usersDAO.FindUserByNameAndPassword(user);
+            // return true if found in the list
+           
+        }
+    }
+}
